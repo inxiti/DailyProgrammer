970c898963c3b052d54689222da7a3fad9ddc822
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-02 03:06:37 -0400

    completed solution

--- 364-easy.clj
+++ 364-easy.clj
@@ -1,16 +1,10 @@
-(ns com.subpxl
-  (:require [clojure.string :as string]))
+(defn roll-dice [s]
+  (let [numbers (clojure.string/split s #"[dD]")
+        dice    (Integer/parseInt (first numbers))
+        sides   (Integer/parseInt (last numbers))
+        rolls   (take dice (repeatedly #(inc (rand-int sides))))
+        total   (apply + rolls)]
+    (str total ": " (clojure.string/join " " rolls))))
 
-; TODO: params should be a string in format "DdS" to be parsed, ex: "3d6"
-(defn roll-dice
-  "Rolls `amount` of dice, with `sides`."
-  [amount sides]
-  (loop [remaining-dice amount
-         rolls []]
-    (if (pos? remaining-dice)
-      (recur (dec remaining-dice)
-             (conj rolls (inc (rand-int sides))))
-      (str (apply + rolls) ": " (string/join " " rolls)))))
+    (roll-dice "3d20")
 
-; (roll-dice "3d20") ; goal
-(roll-dice 5 20)
