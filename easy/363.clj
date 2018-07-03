95a0d57dc33df2e5d33ccc02d4bc8e1c488329e1
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-03 02:00:57 -0400

    solution to 363

---
+++ easy/363.clj
@@ -0,0 +1,11 @@
+(defn check
+  "Returns `true` if 'ei' follows 'c', or if the word does not contain 'ei'."
+  [word]
+  (cond
+    (.contains word "cie") false
+    (and (.contains word "ei")
+         (not (.contains word "cei"))) false
+    :else true))
+
+; test
+(map check ["a" "zombie" "transceiver" "veil" "icier"])
