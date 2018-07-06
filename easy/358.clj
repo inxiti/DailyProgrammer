75549b4bf17da6cdf5071bb420417e2caa9f5410
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-06 00:08:47 -0400

    solution completed

--- easy/358.clj
+++ easy/358.clj
@@ -1,39 +1,45 @@
-; challenge inputs
-;     _  _     _  _  _  _  _ 
-;   | _| _||_||_ |_   ||_||_|
-;   ||_  _|  | _||_|  ||_| _|
-; output: 123456789
-;     _  _  _  _  _  _  _  _ 
-; |_| _| _||_|| ||_ |_| _||_ 
-;   | _| _||_||_| _||_||_  _|
-; output: 433805825
-;  _  _  _  _  _  _  _  _  _ 
-; |_  _||_ |_| _|  ||_ | ||_|
-;  _||_ |_||_| _|  ||_||_||_|
-; output: 526837608
-;  _  _        _  _  _  _  _ 
-; |_||_ |_|  || ||_ |_ |_| _|
-;  _| _|  |  ||_| _| _| _||_ 
-; output: 954105592
-
-(def challenge-two "    _  _  _  _  _  _  _  _ 
+(def challenges
+  {0 "    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|", ; = 123456789
+   1 "    _  _  _  _  _  _  _  _ 
 |_| _| _||_|| ||_ |_| _||_ 
-  | _| _||_||_| _||_||_  _|")
+  | _| _||_||_| _||_||_  _|", ; = 433805825
+   2 " _  _  _  _  _  _  _  _  _ 
+|_  _||_ |_| _|  ||_ | ||_|
+ _||_ |_||_| _|  ||_||_||_|", ; = 526837608
+   3 " _  _        _  _  _  _  _ 
+|_||_ |_|  || ||_ |_ |_| _|
+ _| _|  |  ||_| _| _| _||_ " ; = 954105592
+})
 
-(def legend
-  {" _ | ||_|" 0,
-   "     |  |" 1,
-   " _  _||_ " 2,
-   " _  _| _|" 3,
-   "   |_|  |" 4,
-   " _ |_  _|" 5,
-   " _ |_ |_|" 6,
-   " _   |  |" 7,
-   " _ |_||_|" 8,
-   " _ |_| _|" 9})
+(def legend {" _ | ||_|" 0,
+             "     |  |" 1,
+             " _  _||_ " 2,
+             " _  _| _|" 3,
+             "   |_|  |" 4,
+             " _ |_  _|" 5,
+             " _ |_ |_|" 6,
+             " _   |  |" 7,
+             " _ |_||_|" 8,
+             " _ |_| _|" 9})
 
-; TODO: the string is 27 chars wide, 3 strings total
-; (defn decipher [s])
+(defn decipher
+  "Decipher seven segment display `s`, for nine numbers."
+  [s]
+  (let [input (clojure.string/split s #"\n")
+        numbers 9  ; amount of digits
+        width   3  ; width of digit
+        height  3] ; height of digit
+    (loop [begin  0
+           result ""]
+      (if (> begin (* (dec numbers) width))
+        result
+        (recur (+ begin width)
+               (str result
+                    (legend (apply str
+                                   (map #(subs (input %) begin (+ begin width))
+                                        (range height))))))))))
 
-; (print (get legend "     |  |"))
-(println challenge-two)
+(map (comp decipher val) challenges)
+; => ("123456789" "433805825" "526837608" "954105592")
