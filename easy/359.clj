aef59a7b619ddf0a6e8de2e6ce827cc0fd44063c
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-07 21:34:00 -0400

    comments for new implementation

--- easy/359.clj
+++ easy/359.clj
@@ -6,7 +6,11 @@
     (if (>= counter n)
       sequence
       (recur (inc counter)
+             ; can't this be done with a zip? zipping the first(altered) and
+             ; second(unaltered) easier?
              (str (apply str (map #(str (if (= (mod (first %) 2) 0) "1" "0")
                                         (second %))
                                   (map-indexed vector sequence)))
                   "0")))))
+
+(paper-fold 8)
